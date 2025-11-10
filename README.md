492e81a58d8527a31813c2ca87466c4629e7a620
chaim111404 <natankabalot@gmail.com>
2025-11-10 08:35:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ski2026
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/chaim111404/ski2026)
---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'ski-speed-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+  // אפשר להוסיף כאן גם את האייקונים
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
